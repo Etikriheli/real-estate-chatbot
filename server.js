cbe46dba8d819ff2d574f266ecfb63970383234d
Etikriheli <etikriheli@gmail.com>
2025-06-22 16:33:27 +0300

    server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const port = process.env.PORT || 3000;
 // הגשת קבצים סטטיים
 app.use(express.static('.'));
 
+// route לקבלת הגדרות
+app.get('/config', (req, res) => {
+  res.json({
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY,
+    API_KEY: process.env.API_KEY
+  });
+});
+
 // נתיב ראשי
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
